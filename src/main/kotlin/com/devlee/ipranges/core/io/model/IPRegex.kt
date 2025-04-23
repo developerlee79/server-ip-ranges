1578d3a9b47e58d1887d167df270ebe85c01f881
devle <81211112+developerlee79@users.noreply.github.com>
2025-04-23 22:37:52 +0900

    feat: change regex data type string -> regex

--- src/main/kotlin/com/devlee/ipranges/core/io/model/IPRegex.kt
+++ src/main/kotlin/com/devlee/ipranges/core/io/model/IPRegex.kt
@@ -1,9 +1,6 @@
 package com.devlee.ipranges.core.io.model
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class IPRegex(
     val name: String,
-    val regex: List<String>
+    val regex: List<Regex>
 )
